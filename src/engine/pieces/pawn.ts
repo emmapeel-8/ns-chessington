aaae7d08708c6783f28343234e8bf311b6fc4e51
Emma Peel <Emma.Peel@softwire.com>
2026-09-11 15:32:51 +0100

    Make pawns move forwards and backwards

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,14 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const direction = this.player === Player.WHITE ? 1 : -1;
+        const nextRow = currentSquare.row + direction;
+
+        const moves: Square[] = [];
+        if (nextRow >= 0 && nextRow < GameSettings.BOARD_SIZE) {
+            moves.push(Square.at(nextRow, currentSquare.col));
+        }
+        return moves;
     }
 }
