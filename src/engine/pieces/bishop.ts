7260dbc336c86340a7b7f420765e8c5033f0d506
Emma Peel <Emma.Peel@softwire.com>
2026-09-11 15:50:56 +0100

    Make bishops move diagonally (not rooks)

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,20 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const currentSquare = board.findPiece(this);
+        const directions = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+        const moves: Square[] = [];
+
+        for (const [rowStep, colStep] of directions) {
+            for (let i = 1; i < GameSettings.BOARD_SIZE; i++) {
+                const row = currentSquare.row + i * rowStep;
+                const col = currentSquare.col + i * colStep;
+                if (row < 0 || row >= GameSettings.BOARD_SIZE || col < 0 || col >= GameSettings.BOARD_SIZE) {
+                    break;
+                }
+                moves.push(Square.at(row, col));
+            }
+        }
+        return moves;
     }
 }
